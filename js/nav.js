148e20b492ca4216acb9a6ad67550eb193f79c99
Edwin kipsang <edwinsangkip@gmail.com>
2024-11-13 22:51:40 +0300

    hamberger manu linking

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+const hamb=document.querySelector(".harmburger")
+const nav_menu=document.querySelector(".nav-center")
+const navLink=document.querySelectorAll(".nav-link")
+
+hamb.addEventListener("click",mobileMenu)
+
+function mobileMenu(){
+    hamb.classList.toggle("active");
+    nav_menu.classList.toggle("active")
+}
+
+navLink.forEach(n=>n.addEventListener("click",closemenu));
+
+function closemenu(){hamb.classList.remove("active")
+    nav_menu.classList.remove("active");
+}
