2bf7565640649fe2d8fea78421e1842d7e9faef6
Edwin kipsang <edwinsangkip@gmail.com>
2024-11-14 11:39:28 +0300

    actions initials

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,20 @@
-const hamb=document.querySelector(".harmburger")
-const nav_menu=document.querySelector(".nav-center")
+const hamb=document.getElementById("harmburger")
+const nav_menu=document.getElementById("nav-center")
 const navLink=document.querySelectorAll(".nav-link")
 
-hamb.addEventListener("click",mobileMenu)
+// hamb.addEventListener("click",mobileMenu)
 
-function mobileMenu(){
-    hamb.classList.toggle("active");
+// function mobileMenu(){
+//     hamb.classList.toggle("active");
+//     nav_menu.classList.toggle("active")
+// }
+
+// navLink.forEach(n=>n.addEventListener("click",closemenu));
+
+// function closemenu(){hamb.classList.remove("active")
+//     nav_menu.classList.remove("active");
+// }
+
+hamb.addEventListener("click",()=>{
     nav_menu.classList.toggle("active")
-}
-
-navLink.forEach(n=>n.addEventListener("click",closemenu));
-
-function closemenu(){hamb.classList.remove("active")
-    nav_menu.classList.remove("active");
-}
+})
